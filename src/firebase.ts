fe319fb7134e8d688a65200858378a487b2fcb9e
phtn458 <phtn458@gmail.com>
2019-06-10 00:02:45 -0400

    unexposed api keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const config = {
-  apiKey: "AIzaSyDz8OS8Ha4-GVI9pk_iG22WSDlyK7YPVFY",
-  authDomain: "idle-devs.firebaseapp.com",
-  databaseURL: "https://idle-devs.firebaseio.com",
-  projectId: "idle-devs",
-  storageBucket: "",
-  messagingSenderId: "771431146971",
-  appId: "1:771431146971:web:11340c4eeaa469fa"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const firebase = app.initializeApp(config)
